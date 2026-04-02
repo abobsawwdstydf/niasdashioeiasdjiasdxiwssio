983d111bef2e1871b7e6ef846b35f1f5b2748507
abobsawwdstydf <abobsawwdstydf@github.com>
2026-04-03 00:46:38 +0700

    Update API config for mobile apps21

--- desktop/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose protected methods to renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  getServerUrl: () => ipcRenderer.invoke('get-server-url'),
-  getPlatform: () => ipcRenderer.invoke('get-platform'),
-  platform: process.platform
-});
