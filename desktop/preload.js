ea05ee6cb9be4cc7b975950e2a0de630ec571073
abobsawwdstydf <abobsawwdstydf@github.com>
2026-04-03 13:24:08 +0700

    Update

--- desktop/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  getServerUrl: () => ipcRenderer.invoke('get-server-url'),
-  getPlatform: () => ipcRenderer.invoke('get-platform'),
-  platform: process.platform
-});
